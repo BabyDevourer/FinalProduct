fde533efbbc849c148c91616d987067e4aca552f
Tony <tony@pnholdings.com>
2024-11-03 10:18:29 +0700

    Initial Commit

---
+++ Final Product 3Tests/Final_Product_3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Final_Product_3Tests.swift
+//  Final Product 3Tests
+//
+//  Created by admin on 3/11/24.
+//
+
+import Testing
+@testable import Final_Product_3
+
+struct Final_Product_3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
